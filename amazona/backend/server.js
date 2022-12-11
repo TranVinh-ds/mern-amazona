d241b9720886fb33840c93cdb2e9b68644a9190d
TranVinh-ds <chenvinh2003@gmail.com>
2022-12-12 00:25:41 +0700

    Add-product-screen

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -7,6 +7,15 @@ app.get('/api/products', (request, response) => {
   response.send(data.products);
 });
 
+app.get('/api/products/slug/:slug', (request, response) => {
+  const product = data.products.find((x) => x.slug === request.params.slug);
+  if (product) {
+    response.send(product);
+  } else {
+    response.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
